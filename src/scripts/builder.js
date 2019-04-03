095e2e1a9c620254e3ab1a61d3bc16b71d1d3bfb
Alexey Usanin <aleksej.usanin@icloud.com>
2019-04-03 22:28:02 +0300

    Added jquery

--- src/scripts/builder.js
+++ src/scripts/builder.js
@@ -1,8 +1,7 @@
+const $ = require('jquery');
 
 module.exports = (tag = 'div', content = '', className = 'box') => {
-  const element = document.createElement(tag);
-  element.className = className;
-  element.innerHTML = content;
+  const element = $(`<${tag}></${tag}>`).addClass(className).html(content);
 
   return element;
 };
