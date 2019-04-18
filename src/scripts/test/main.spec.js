f89cf30d98069d3a05bffc90ecd847fd76f86c50
Alexey Usanin <aleksej.usanin@icloud.com>
2019-04-18 22:09:49 +0300

    Add Unit tests

--- src/scripts/test/main.spec.js
+++ src/scripts/test/main.spec.js
@@ -1,8 +1,9 @@
 import chai from 'chai';
 import sinon from 'sinon';
-const { expect, assert } = chai;
+const { expect } = chai;
 chai.should();
 import { showMessage, getDay, getAdultUsers, getRandomUsers} from './main';
+import { days, defaultProduct, money, users } from './constants';
 import { Product } from './main';
 
 describe('Test main.js functions', () => {
@@ -15,13 +16,83 @@ describe('Test main.js functions', () => {
   });
 
   it('should return current day', () => {
-    getDay().should.equal('Sunday');
+    getDay().should.equal(days[new Date().getDay()]);
   });
 
-  it('should return getAdultUsers()', () => {
-    const users = [{age: 15}, {age: 68}, {age: 38}, {age: 18}, {age: 22}, {age: 14}];
-    const adultUser = getAdultUsers(users).length;
-    expect(adultUser).to.equal(3);
+  it('getAdultUsers() should return Array', () => {
+    getAdultUsers().should.instanceof(Array);
   });
   
+  it('getAdultUsers() should return users is older than 18 year', () => {
+    expect(getAdultUsers(users)).to.deep.equal([{age: 28}, {age: 45}, {age: 68}, {age: 38}, {age: 22}]);  
+  });
+
+  describe('Test getRandomUser function', () => {
+    let stubRandom = sinon.stub(Math, 'random');
+    let stubRound = sinon.stub(Math, 'round');
+    let middleUser = Math.round(users.length / 2);
+
+    it('should return false if empty', () => {
+      getRandomUsers().should.equal(false);
+    });
+
+    it('return first users if number bigger than 0.5', () => {
+      stubRandom.returns(0.6);
+      expect(getRandomUsers(users)).to.deep.equal(users.slice(0, middleUser));
+      stubRandom.restore();
+    });
+
+    it('return last users if number smaller than 0.5', () => {
+      stubRandom.returns(0.2);
+      expect(getRandomUsers(users)).to.deep.equal(users.slice(middleUser, middleUser));
+      stubRandom.restore();
+    });
+  });
+
+  describe('Test Product class', () => {
+    let product;
+    let price;
+    let title;
+
+    beforeEach(() => {
+      product = new Product();
+    });
+
+    it('should create class with title and price', () => {
+      title = 'Orange';
+      price = 14;
+      product = new Product(title, price);
+      product.title.should.equal(title);
+      product.price.should.equal(price);
+      console.log(product);
+    });
+
+    it('should return default title and price', () => {
+      let title;
+      let price;
+      product = new Product(title, price);
+      product.title.should.equal(defaultProduct);
+      product.price.should.equal(10);
+      console.log(product);
+    });
+
+    it('getPrice() should return price with money', () => {
+      product.getPrice().should.equal('10$');
+    });
+
+    it('should throw error if empty value', () => {
+      expect(product.setPrice).to.throw(Error, 'Price should be defined');
+    });
+
+    it('should set price if value bigger 10', () => {
+      let value = 42;
+      product.setPrice(value);
+      product.value.should.equal(42);
+    });
+
+    it('should set default price if value smaller 10', () => {
+      expect(product.setPrice(5)).should.not.to.equal(5);
+    });
+  });
+
 });
--- src/scripts/test/tools.js
+++
@@ -1,2 +0,0 @@
-// export const sum = (a = 0, b = 0) => a + b;
-// console.log(sum(4, 4));
