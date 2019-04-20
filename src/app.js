f5cedbc63e8af8ef3ba33e83c5af33d0f79140b8
Alexey Usanin <aleksej.usanin@icloud.com>
2019-04-20 15:23:17 +0300

    Add linter

--- src/app.js
+++ src/app.js
@@ -1,15 +1,21 @@
-import React from 'react';
 import ReactDom from 'react-dom';
-import { Navigation } from './navigation/Navigation';
+import { Navigation } from './navigation';
 
 
 const Time = () => <time>{String(new Date().toLocaleDateString())}</time>;
 
-const header = (
+const App = () => (
   <header>
-    <h1>Hello <Time /></h1>
-    <Navigation/> 
+    <h1>
+Hello
+      <Time />
+    </h1>
+    <Navigation />
   </header>
 );
 
-ReactDom.render(header, document.getElementById('app'))
+ReactDom.render(<App />, document.getElementById('app'));
+
+if (module.hot) {
+  module.hot.accept();
+}
--- src/navigation/Navigation.js
+++ src/navigation/Navigation.js
@@ -1,15 +1,32 @@
-import React from 'react';
 import './navigation.css';
+
+const Error = ({ text = 'error', color = 'red' }) => <mark style={{ color }}>{text}</mark>;
+const names = ['John', 'Ben'];
+
+const Names = () => (
+  <ul>
+    {
+      names
+        .sort((prev, next) => (prev < next ? 1 : -1))
+        .map(name => <li key={name}>{name}</li>)
+    }
+  </ul>
+);
 
 export const Navigation = () => (
   <nav className="nav">
     <ul>
       <li>
-        <a>One</a>
+        <a href="/one">One</a>
       </li>
       <li>
-        <a>Two</a>
+        <a href="/two">Two</a>
       </li>
     </ul>
+    <Error
+      text="some text"
+      color="red"
+    />
+    <Names />
   </nav>
 );
