3b3ff4afb4a8b43f4dcfb21db4183e1200ee09f0
Alexey Usanin <aleksej.usanin@icloud.com>
2019-05-03 18:29:35 +0300

    Create get post function

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Numbers } from '../numbers/Numbers';
-import { UserList, users } from '../users/Users';
+import { UserList } from '../users/Users';
 import { Counter } from '../counter/Counter';
 import { Button } from '../button/Button';
 import { ToggleContent } from '../button/ToggleButton';
 import { TaskList } from '../task/Task';
+import { PostList } from '../users/Post';
 import './main.scss';
 
 const Aside = () => (
@@ -17,18 +18,41 @@ const Aside = () => (
 );
 
 export class Main extends Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: [],
+      posts: []
+    };
+    this.getUsers();
+    this.getPost();
+
+  }
+  
+  getUsers() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({users}));
+  }
+  
+  getPost = (id) => {
+    fetch(`https://jsonplaceholder.typicode.com/posts?userId=${id}`)
+    .then(resp=> resp.json())
+    .then(posts => this.setState({posts}));
+  }
+  
   render() {
+
+    const { users, posts } = this.state;
+  
     return (
       <main className="main">
         <Aside />
         <div className="main-content">
           <h2>Content</h2>
-          <Numbers
-            from={2}
-            to={19}
-            even
-          />
-          <UserList list={users} name="name" lastName="lastName" age="age" />
+          <UserList list={users} onClick={this.getPost}/>
+          <PostList posts={posts}  />
           <Counter />
           <Button />
           <ToggleContent />
--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,32 +1,26 @@
-export const users = [
-  { name: 'John', lastName: 'Petrov', age: 45 },
-  { name: 'Bill', lastName: 'Ivanov', age: 23 }
-];
 
-const User = params => (
-  <li>
-    {'Name - '}
-    {params.name}
-    <br />
-    {'Last Name - '}
-    {params.lastName}
-    <br />
-    {'Age - '}
-    {params.age}
+const User = ({ id, name, onClick }) => 
+  
+  (
+  <li key={id} onClick={() => onClick(id)}>
+    {name}
   </li>
 );
 
-export const UserList = props => (
-  <ul>
-    {
-    props.list.map((item, i) => (
-      <User
-        key={i}
-        name={item[props.name]}
-        lastName={item[props.lastName]}
-        age={item[props.age]}
-      />
-    ))
- }
-  </ul>
+export const UserList = ({list, onClick}) => (
+  <div>
+    <ul>
+      {
+      list.map(({id, name}) => (
+        <User
+          id={id}
+          key={id}
+          name={name}
+          onClick={onClick}
+        />
+      ))
+  }
+    </ul>
+  </div>
 );
+
